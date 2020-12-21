377b2cbab8355b7843c74a0dd5989f72c57275bb
vitalii <v@ushal.ru>
2020-12-21 10:00:35 +0300

    Revert change test failure

--- test/CommandAPI.Tests/CommandsControllerTests.cs
+++ test/CommandAPI.Tests/CommandsControllerTests.cs
@@ -272,8 +272,8 @@ namespace CommandAPI.Tests
             //Act
             var result = controller.DeleteCommand(0);
             //Assert
-            // Assert.IsType<NotFoundResult>(result);
-            Assert.IsType<OkResult>(result);
+            Assert.IsType<NotFoundResult>(result);
+            // Assert.IsType<OkResult>(result);
         }
     }
 }
